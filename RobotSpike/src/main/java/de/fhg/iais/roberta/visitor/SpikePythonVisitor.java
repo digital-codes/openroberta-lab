bf3939676548e9e408917606d657f2c4148216f5
Beate Jost <beate.jost@iais.fraunhofer.de>
2022-10-18 13:33:34 +0200

    init Spike Prime

---
+++ RobotSpike/src/main/java/de/fhg/iais/roberta/visitor/SpikePythonVisitor.java
@@ -0,0 +1,223 @@
+package de.fhg.iais.roberta.visitor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.google.common.collect.ClassToInstanceMap;
+
+import de.fhg.iais.roberta.bean.CodeGeneratorSetupBean;
+import de.fhg.iais.roberta.bean.IProjectBean;
+import de.fhg.iais.roberta.bean.UsedHardwareBean;
+import de.fhg.iais.roberta.components.ConfigurationAst;
+import de.fhg.iais.roberta.components.UsedSensor;
+import de.fhg.iais.roberta.syntax.Phrase;
+import de.fhg.iais.roberta.syntax.configuration.ConfigurationComponent;
+import de.fhg.iais.roberta.syntax.lang.blocksequence.MainTask;
+import de.fhg.iais.roberta.syntax.lang.expr.ColorConst;
+import de.fhg.iais.roberta.syntax.lang.expr.ConnectConst;
+import de.fhg.iais.roberta.syntax.lang.expr.RgbColor;
+import de.fhg.iais.roberta.syntax.lang.stmt.StmtList;
+import de.fhg.iais.roberta.syntax.lang.stmt.WaitStmt;
+import de.fhg.iais.roberta.syntax.lang.stmt.WaitTimeStmt;
+import de.fhg.iais.roberta.syntax.sensor.generic.TimerReset;
+import de.fhg.iais.roberta.syntax.sensor.generic.TimerSensor;
+import de.fhg.iais.roberta.util.syntax.SC;
+import de.fhg.iais.roberta.visitor.lang.codegen.prog.AbstractPythonVisitor;
+
+/**
+ * This class is implementing {@link IVisitor}. All methods are implemented and they append a human-readable Python code representation of a phrase to a
+ * StringBuilder. <b>This representation is correct Python code.</b> <br>
+ */
+public final class SpikePythonVisitor extends AbstractPythonVisitor implements ISpikeVisitor<Void> {
+
+    private final ConfigurationAst configurationAst;
+    private String rightMotorPort;
+
+    /**
+     * initialize the Python code generator visitor.
+     *
+     * @param programPhrases to generate the code from
+     */
+    public SpikePythonVisitor(
+        List<List<Phrase>> programPhrases, ClassToInstanceMap<IProjectBean> beans, ConfigurationAst configurationAst) {
+        super(programPhrases, beans);
+        this.configurationAst = configurationAst;
+    }
+
+
+    @Override
+    protected void generateProgramPrefix(boolean withWrapping) {
+        if ( !withWrapping ) {
+            return;
+        }
+        /*this.sb.append("import spike");
+        if ( this.getBean(UsedHardwareBean.class).isActorUsed(SC.ENCODER) ) {
+            this.sb.append(", mbot2");
+        }
+        if ( this.getBean(UsedHardwareBean.class).isSensorUsed(SpikeConstants.MBUILDSENSOR) ) {
+            this.sb.append(", mbuild");
+        }
+        nlIndent();
+        this.sb.append("import time");
+        nlIndent();
+        this.sb.append("import math, random");
+        nlIndent();
+        if ( this.getBean(UsedHardwareBean.class).isActorUsed(SC.DIFFERENTIALDRIVE) ) {
+            appendRobotVariables();
+        }
+        generateTimerVariables();
+        addColors();*/
+        if ( !this.getBean(CodeGeneratorSetupBean.class).getUsedMethods().isEmpty() ) {
+            String helperMethodImpls =
+                this.getBean(CodeGeneratorSetupBean.class)
+                    .getHelperMethodGenerator()
+                    .getHelperMethodDefinitions(this.getBean(CodeGeneratorSetupBean.class).getUsedMethods());
+            this.sb.append(helperMethodImpls);
+        }
+    }
+
+    private void appendRobotVariables() {
+       /* ConfigurationComponent diffDrive = getDiffDrive();
+        if ( diffDrive != null ) {
+            nlIndent();
+            double circumference = Double.parseDouble(diffDrive.getComponentProperties().get(SpikeConstants.DIFF_WHEEL_DIAMETER)) * Math.PI;
+            double trackWidth = Double.parseDouble(diffDrive.getComponentProperties().get(SpikeConstants.DIFF_TRACK_WIDTH));
+            this.sb.append("_trackWidth = ");
+            this.sb.append(trackWidth);
+            nlIndent();
+            this.sb.append("_circumference = ");
+            this.sb.append(circumference);
+            nlIndent();
+            this.sb.append("_diffPortsSwapped = ");
+            this.sb.append(this.rightMotorPort.equals("EM1") ? "True" : "False");
+            nlIndent();
+        }*/
+    }
+
+    private void generateTimerVariables() {
+        this.getBean(UsedHardwareBean.class)
+            .getUsedSensors()
+            .stream()
+            .filter(usedSensor -> usedSensor.getType().equals(SC.TIMER))
+            .collect(Collectors.groupingBy(UsedSensor::getPort))
+            .keySet()
+            .forEach(port -> {
+                this.usedGlobalVarInFunctions.add("_timer" + port);
+                this.sb.append("_timer").append(port).append(" = spike.timer.get()");
+                nlIndent();
+            });
+    }
+
+    @Override
+    public Void visitMainTask(MainTask mainTask) {
+        StmtList variables = mainTask.variables;
+        variables.accept(this);
+        generateUserDefinedMethods();
+        nlIndent();
+        this.sb.append("def run():");
+        incrIndentation();
+        if ( !this.usedGlobalVarInFunctions.isEmpty() ) {
+            nlIndent();
+            this.sb.append("global ").append(String.join(", ", this.usedGlobalVarInFunctions));
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void generateProgramSuffix(boolean withWrapping) {
+        if ( !withWrapping ) {
+            return;
+        }
+        decrIndentation(); // everything is still indented from main program
+        nlIndent();
+        nlIndent();
+        this.sb.append("def main():");
+        incrIndentation();
+        nlIndent();
+        this.sb.append("try:");
+        incrIndentation();
+        nlIndent();
+        this.sb.append("run()");
+        decrIndentation();
+        nlIndent();
+        this.sb.append("except Exception as e:");
+        incrIndentation();
+        nlIndent();
+        this.sb.append("spike.display.show_label(\"Exeption on Mbot 2\", 16, int(8 * 0 + 5), int(17 * 0))");
+        nlIndent();
+        this.sb.append("spike.display.show_label(e, 16, int(8 * 0 + 5), int(17 * 1))");
+        nlIndent();
+        this.sb.append("raise");
+        decrIndentation();
+        if ( this.getBean(UsedHardwareBean.class).isActorUsed(SC.ENCODER) ) {
+            nlIndent();
+            this.sb.append("finally:");
+            incrIndentation();
+            nlIndent();
+            this.sb.append("mbot2.motor_stop(\"all\")");
+            nlIndent();
+            this.sb.append("mbot2.EM_stop(\"all\")");
+            decrIndentation();
+        }
+        decrIndentation();
+        nlIndent();
+
+        this.sb.append("main()");
+    }
+
+    private String getPortFromConfig(String name) {
+        ConfigurationComponent block = configurationAst.getConfigurationComponent(name);
+        return block.getComponentProperties().get("PORT1");
+    }
+
+    @Override
+    public Void visitWaitStmt(WaitStmt waitStmt) {
+        this.sb.append("while True:");
+        incrIndentation();
+        visitStmtList(waitStmt.statements);
+        decrIndentation();
+        return null;
+    }
+
+    @Override
+    public Void visitWaitTimeStmt(WaitTimeStmt waitTimeStmt) {
+        this.sb.append("time.sleep(");
+        waitTimeStmt.time.accept(this);
+        this.sb.append("/1000)");
+        return null;
+    }
+
+    @Override
+    public Void visitColorConst(ColorConst colorConst) {
+        this.sb.append("(").append(colorConst.getRedChannelInt()).append(", ").append(colorConst.getGreenChannelInt()).append(", ").append(colorConst.getBlueChannelInt()).append(")");
+        return null;
+    }
+
+    @Override
+    public Void visitConnectConst(ConnectConst connectConst) {
+        return null;
+    }
+
+    @Override
+    public Void visitRgbColor(RgbColor rgbColor) {
+        this.sb.append("(");
+        rgbColor.R.accept(this);
+        this.sb.append(", ");
+        rgbColor.G.accept(this);
+        this.sb.append(", ");
+        rgbColor.B.accept(this);
+        this.sb.append(")");
+        return null;
+    }
+
+    @Override
+    public Void visitTimerSensor(TimerSensor timerSensor) {
+        return null;
+    }
+
+    @Override
+    public Void visitTimerReset(TimerReset timerReset) {
+        return null;
+    }
+}
---
+++ RobotSpike/src/main/java/de/fhg/iais/roberta/visitor/SpikeValidatorAndCollectorVisitor.java
@@ -0,0 +1,73 @@
+package de.fhg.iais.roberta.visitor;
+
+import java.util.List;
+
+import com.google.common.collect.ClassToInstanceMap;
+
+import de.fhg.iais.roberta.bean.IProjectBean;
+import de.fhg.iais.roberta.components.ConfigurationAst;
+import de.fhg.iais.roberta.components.UsedSensor;
+import de.fhg.iais.roberta.syntax.action.motor.MotorGetPowerAction;
+import de.fhg.iais.roberta.syntax.action.motor.MotorSetPowerAction;
+import de.fhg.iais.roberta.syntax.configuration.ConfigurationComponent;
+import de.fhg.iais.roberta.syntax.lang.blocksequence.MainTask;
+import de.fhg.iais.roberta.syntax.sensor.generic.TimerReset;
+import de.fhg.iais.roberta.syntax.sensor.generic.TimerSensor;
+import de.fhg.iais.roberta.util.syntax.SC;
+import de.fhg.iais.roberta.visitor.validate.DifferentialMotorValidatorAndCollectorVisitor;
+
+
+public class SpikeValidatorAndCollectorVisitor extends DifferentialMotorValidatorAndCollectorVisitor implements ISpikeVisitor<Void> {
+
+
+    public SpikeValidatorAndCollectorVisitor(ConfigurationAst robotConfiguration, ClassToInstanceMap<IProjectBean.IBuilder> beanBuilders) {
+        super(robotConfiguration, beanBuilders);
+    }
+
+    @Override
+    public Void visitMainTask(MainTask mainTask) {
+        requiredComponentVisited(mainTask, mainTask.variables);
+        return null;
+    }
+
+    @Override
+    public Void visitTimerSensor(TimerSensor timerSensor) {
+        usedHardwareBuilder.addUsedSensor(new UsedSensor(timerSensor.getUserDefinedPort(), SC.TIMER, timerSensor.getMode()));
+        return null;
+    }
+
+    @Override
+    public Void visitTimerReset(TimerReset timerReset) {
+        usedHardwareBuilder.addUsedSensor(new UsedSensor(timerReset.getUserDefinedPort(), SC.TIMER, timerReset.getMode()));
+        return null;
+    }
+
+    @Override
+    public Void visitMotorGetPowerAction(MotorGetPowerAction motorGetPowerAction) {
+        addWarningToPhrase(motorGetPowerAction, "BLOCK_NOT_SUPPORTED");
+        return null;
+    }
+
+    @Override
+    public Void visitMotorSetPowerAction(MotorSetPowerAction motorSetPowerAction) {
+        addWarningToPhrase(motorSetPowerAction, "BLOCK_NOT_SUPPORTED");
+        return null;
+    }
+
+    private ConfigurationComponent getSubComponent(String userDefinedPort) {
+        for ( ConfigurationComponent component : this.robotConfiguration.getConfigurationComponentsValues() ) {
+            try {
+                for ( List<ConfigurationComponent> subComponents : component.getSubComponents().values() ) {
+                    for ( ConfigurationComponent subComponent : subComponents ) {
+                        if ( subComponent.userDefinedPortName.equals(userDefinedPort) ) {
+                            return subComponent;
+                        }
+                    }
+                }
+            } catch ( UnsupportedOperationException e ) {
+                continue;
+            }
+        }
+        return null;
+    }
+}
