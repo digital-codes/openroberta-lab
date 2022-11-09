3539b61e48eb1d09121db1d6b6217a0c9f14103d
heini208 <mstemmeler@gmx.de>
2022-11-09 14:46:43 +0100

    change optical sensor ast blocklyName to fix server error

--- RobotRobotino/src/main/java/de/fhg/iais/roberta/syntax/sensor.robotino/OpticalSensor.java
+++ RobotRobotino/src/main/java/de/fhg/iais/roberta/syntax/sensor.robotino/OpticalSensor.java
@@ -6,7 +6,7 @@ import de.fhg.iais.roberta.transformer.forClass.NepoExpr;
 import de.fhg.iais.roberta.util.ast.BlocklyProperties;
 import de.fhg.iais.roberta.util.ast.ExternalSensorBean;
 
-@NepoExpr(category = "SENSOR", blocklyNames = {"robConf_optical_getSample"}, name = "OPTICAL_SENSING",
+@NepoExpr(category = "SENSOR", blocklyNames = {"robSensors_optical_getSample"}, name = "OPTICAL_SENSING",
     sampleValues = {@F2M(field = "OPTICAL_OPENING", mode = "OPENING"), @F2M(field = "OPTICAL_CLOSING", mode = "CLOSING")})
 public final class OpticalSensor extends ExternalSensor {
     public OpticalSensor(BlocklyProperties properties, ExternalSensorBean externalSensorBean) {
